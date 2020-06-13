6f105a00599b0c056f423869b66db5f847dfbd4e
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-06-13 13:52:46 -0600

    included launch json

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,15 @@
 module.exports = {
   // api path is defined using the object key name
+
+  // api/v1/admin
   "admin": require("./admin"),
+
+  // api/v1/customers
   "customers": require("./customers"),
+
+  // api/v1/bus-routes
   "bus-routes": require("./busRoutes"),
+
+  // api/v1/bus-drivers
   "bus-drivers": require("./busDrivers")
 };
