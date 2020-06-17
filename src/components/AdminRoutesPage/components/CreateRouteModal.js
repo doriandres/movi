ed2732ebfc898f66a80489b0afb52ec00f8f2f1b
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-06-17 15:48:19 -0600

    can create routes

--- src/components/AdminRoutesPage/components/CreateRouteModal.js
+++ src/components/AdminRoutesPage/components/CreateRouteModal.js
@@ -1,122 +1,156 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import { useForm, Controller } from 'react-hook-form';
+import { post } from "axios";
 import { useMediaQuery, useTheme, FormControl, InputLabel, Select, MenuItem, FormHelperText } from '@material-ui/core';
 
 import useStyles from "./../../shared/styles/forms";
+import { API_URL } from '../../../settings';
 
-export default function CreateRouteModal({ open, onClose }) {
+export default function CreateRouteModal({ open, onClose = () => { }, onCreated = () => { } }) {
   const classes = useStyles();
   const form = useForm();
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const fullScreen = useMediaQuery(theme.breakpoints.down('xs'));
 
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const cleanError = () => {
+    setError(null);
+  };
+
+  const onSuccess = () => {
+    onCreated();
+    onClose();
+  };
+
+  const onFail = (error) => {
+    setError(error);
+    setLoading(false);
+  };
 
   const onSubmit = (data) => {
-    console.dir(data);
+    setLoading(true);
+    post(`${API_URL}api/v1/bus-routes/insert`, data, { withCredentials: true })
+      .then((response) => onSuccess(response.data.result))
+      .catch(error => onFail(error.response?.data?.error || 'Hubo un error de conexión'));
   };
 
   return (
-
-    <Dialog open={!!open} maxWidth="md" fullWidth scroll="paper" fullScreen={fullScreen}>
-      <DialogTitle>Agregar Ruta</DialogTitle>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        autoComplete="off">
-        <DialogContent>
-          <div>
-            <TextField
-              label="Nombre"
-              name="name"
-              inputRef={form.register({ required: "Debe insertar un nombre" })}
-              variant="outlined"
-              error={!!form.errors.name}
-              helperText={form.errors.name?.message}
-              fullWidth
-            />
-          </div>
-          <div className={classes.marginTop}>
-            <TextField
-              label="Código de Transporte"
-              name="code"
-              inputRef={form.register({ required: "Debe insertar un código de transporte" })}
-              variant="outlined"
-              error={!!form.errors.code}
-              helperText={form.errors.code?.message}
-              fullWidth
-            />
-          </div>
-          <div className={classes.marginTop}>
-            <TextField
-              label="Costo"
-              name="cost"
-              inputRef={form.register({
-                required: "Debe insertar un costo",
-                validate(val) {
-                  const number = parseFloat(val);
-                  if (Number.isNaN(number)) {
-                    return "Debe insertar un costo válido";
+    <>
+      <Dialog open={!!open} maxWidth="md" fullWidth scroll="paper" fullScreen={fullScreen}>
+        <DialogTitle>Agregar Ruta</DialogTitle>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          autoComplete="off">
+          <DialogContent>
+            <div>
+              <TextField
+                label="Nombre"
+                name="name"
+                inputRef={form.register({ required: "Debe insertar un nombre" })}
+                variant="outlined"
+                error={!!form.errors.name}
+                helperText={form.errors.name?.message}
+                fullWidth
+              />
+            </div>
+            <div className={classes.marginTop}>
+              <TextField
+                label="Código de Transporte"
+                name="code"
+                inputRef={form.register({ required: "Debe insertar un código de transporte" })}
+                variant="outlined"
+                error={!!form.errors.code}
+                helperText={form.errors.code?.message}
+                fullWidth
+              />
+            </div>
+            <div className={classes.marginTop}>
+              <TextField
+                label="Costo"
+                name="cost"
+                inputRef={form.register({
+                  required: "Debe insertar un costo",
+                  validate(val) {
+                    const number = parseFloat(val);
+                    if (Number.isNaN(number)) {
+                      return "Debe insertar un costo válido";
+                    }
                   }
+                })}
+                variant="outlined"
+                error={!!form.errors.cost}
+                helperText={form.errors.cost?.message}
+                fullWidth
+              />
+            </div>
+            <FormControl fullWidth variant="outlined" className={classes.marginTop}>
+              <InputLabel id="provice-select" error={!!form.errors.province}>Provincia</InputLabel>
+              <Controller
+                control={form.control}
+                name="province"
+                rules={{ required: "Debe elegir una provincia" }}
+                as={
+                  <Select
+                    labelId="provice-select"
+                    label="Provincia"
+                    error={!!form.errors.province}
+                  >
+                    <MenuItem value="San José">San José</MenuItem>
+                    <MenuItem value="Alajuela">Alajuela</MenuItem>
+                    <MenuItem value="Cartago">Cartago</MenuItem>
+                    <MenuItem value="Heredia">Heredia</MenuItem>
+                    <MenuItem value="Guanacaste">Guanacaste</MenuItem>
+                    <MenuItem value="Puntarenas">Puntarenas</MenuItem>
+                    <MenuItem value="Limón">Limón</MenuItem>
+                  </Select>
                 }
-              })}
-              variant="outlined"
-              error={!!form.errors.cost}
-              helperText={form.errors.cost?.message}
-              fullWidth
-            />
-          </div>
-          <FormControl fullWidth variant="outlined" className={classes.marginTop}>
-            <InputLabel id="provice-select" error={!!form.errors.province}>Provincia</InputLabel>
-            <Controller
-              control={form.control}
-              name="province"
-              rules={{ required: "Debe elegir una provincia" }}
-              as={
-                <Select
-                  labelId="provice-select"
-                  label="Provincia"
-                  error={!!form.errors.province}
-                >
-                  <MenuItem value="San José">San José</MenuItem>
-                  <MenuItem value="Alajuela">Alajuela</MenuItem>
-                  <MenuItem value="Cartago">Cartago</MenuItem>
-                  <MenuItem value="Heredia">Heredia</MenuItem>
-                  <MenuItem value="Guanacaste">Guanacaste</MenuItem>
-                  <MenuItem value="Puntarenas">Puntarenas</MenuItem>
-                  <MenuItem value="Limón">Limón</MenuItem>
-                </Select>
-              }
-            />
-            <FormHelperText error={!!form.errors.province}>{form.errors.province?.message}</FormHelperText>
-          </FormControl>
-          <div className={classes.marginTop}>
-            <TextField
-              label="Cantón"
-              name="canton"
-              inputRef={form.register({ required: "Debe insertar un cantón" })}
-              variant="outlined"
-              error={!!form.errors.canton}
-              helperText={form.errors.canton?.message}
-              fullWidth
-            />
-          </div>
+              />
+              <FormHelperText error={!!form.errors.province}>{form.errors.province?.message}</FormHelperText>
+            </FormControl>
+            <div className={classes.marginTop}>
+              <TextField
+                label="Cantón"
+                name="canton"
+                inputRef={form.register({ required: "Debe insertar un cantón" })}
+                variant="outlined"
+                error={!!form.errors.canton}
+                helperText={form.errors.canton?.message}
+                fullWidth
+              />
+            </div>
+          </DialogContent>
+          <DialogActions className={classes.marginTop}>
+            <Button disabled={loading} type="submit" variant="contained" color="primary">
+              {loading ? 'Guardando' : 'Guardar'}
+            </Button>
+            <Button disabled={loading} onClick={onClose} variant="outlined">
+              Descartar
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+      <Dialog open={!!error} onClose={cleanError} maxWidth="xs" fullWidth>
+        <DialogTitle>Lo sentimos</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {error}
+          </DialogContentText>
         </DialogContent>
-        <DialogActions className={classes.marginTop}>
-          <Button onClick={onClose} color="primary">
-            Descartar
-          </Button>
-          <Button type="submit" color="inherit">
-            Guardar
+        <DialogActions>
+          <Button onClick={cleanError} color="primary">
+            Aceptar
           </Button>
         </DialogActions>
-      </form>
-    </Dialog >
+      </Dialog>
+    </>
   );
 }
--- src/locations.js
+++ src/locations.js
@@ -3,4 +3,4 @@ export const NOT_FOUND = () => '/404';
 export const ADMIN_SIGN_IN = () => '/admin/iniciar-sesion';
 export const SIGN_IN = () => '/iniciar-sesion';
 export const ADMIN_LANDING = () => '/admin';
-export const ADMIN_ROUTES = () => '/admin/routes';
+export const ADMIN_ROUTES = () => '/admin/rutas';
