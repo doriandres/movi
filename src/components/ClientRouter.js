eb0abf26f171330e92aa8f8d500666c56b47a1f4
PabloM24 <pama24@hotmail.es>
2020-06-20 13:31:58 -0600

    Revert "Adding Admin Landing Page Route"
    This reverts commit d61b2f13ebf0e52e6dc6a16460452463ea0b6350.

--- src/components/ClientRouter.js
+++ src/components/ClientRouter.js
@@ -1,13 +1,12 @@
 import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { HOME_PAGE, NOT_FOUND, ADMIN_SIGN_IN, SIGN_IN, ADMIN_LANDING_PAGE } from './../locations';
+import { HOME_PAGE, NOT_FOUND, ADMIN_SIGN_IN, SIGN_IN } from './../locations';
 import Loading from "./Loading";
 
 const HomePage = lazy(() => import("./HomePage"));
 const NotFoundPage = lazy(() => import("./NotFoundPage"));
 const AdminSignInPage = lazy(() => import("./AdminSignInPage"));
 const SignInPage = lazy(() => import("./SignInPage"));
-const AdminLandingPage = lazy(() => import("./AdminLandingPage"));
 
 export default function ClientRouter() {
   return (
@@ -17,7 +16,6 @@ export default function ClientRouter() {
         <Route exact path={NOT_FOUND()} component={NotFoundPage} />
         <Route exact path={ADMIN_SIGN_IN()} component={AdminSignInPage} />
         <Route exact path={SIGN_IN()} component={SignInPage} />
-        <Route exact path={ADMIN_LANDING_PAGE()} component={AdminLandingPage} />
         <Route path="*">
           <Redirect to="/404" />
         </Route>
--- src/locations.js
+++ src/locations.js
@@ -2,4 +2,3 @@ export const HOME_PAGE = () => '/';
 export const NOT_FOUND = () => '/404';
 export const ADMIN_SIGN_IN = () => '/admin/iniciar-sesion';
 export const SIGN_IN = () => '/iniciar-sesion';
-export const ADMIN_LANDING_PAGE = () => '/admin';
