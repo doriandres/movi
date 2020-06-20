d61b2f13ebf0e52e6dc6a16460452463ea0b6350
PabloM24 <pama24@hotmail.es>
2020-06-20 13:20:16 -0600

    Adding Admin Landing Page Route

---
+++ src/components/AdminLandingPage/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button, Paper, Grid, Typography } from '@material-ui/core';
+import useStyles from "../shared/styles/forms";
+
+export default function AdminLandingPage() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} md={4} />
+        <Grid item xs={12} md={4}>
+          <Paper className={classes.padding}>
+            <Typography
+              className={classes.noMarginTop}
+              variant="h5"
+            >
+              Adminstración Landing Page
+            </Typography>
+            <form className={classes.marginTop} noValidate autoComplete="off">
+              <div>
+                <TextField
+                  label="Correo Electrónico"
+                  variant="outlined"
+                  // helperText="Debe insertar un correo electrónico"
+                  // error
+                  fullWidth
+                />
+              </div>
+              <div className={classes.marginTop}>
+                <TextField
+                  label="Contraseña"
+                  variant="outlined"
+                  // helperText="Debe insertar una contraseña"
+                  // error
+                  fullWidth
+                />
+              </div>
+              <div className={classes.marginTop}>
+                <Button variant="contained" color="primary" size="large" fullWidth>Iniciar Sesión</Button>
+              </div>
+            </form>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
--- src/locations.js
+++ src/locations.js
@@ -2,3 +2,4 @@ export const HOME_PAGE = () => '/';
 export const NOT_FOUND = () => '/404';
 export const ADMIN_SIGN_IN = () => '/admin/iniciar-sesion';
 export const SIGN_IN = () => '/iniciar-sesion';
+export const ADMIN_LANDING_PAGE = () => '/admin';
