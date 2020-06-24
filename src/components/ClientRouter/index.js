12713e7c3ed682f08a368ce77ac16157037f1c14
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2020-06-24 12:28:28 -0600

    Home page redirects to admin sign in

--- src/components/ClientRouter/index.js
+++ src/components/ClientRouter/index.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { NOT_FOUND, ADMIN_SIGN_IN, ADMIN_LANDING, ADMIN_ROUTES, ADMIN_DRIVERS, ADMIN_USERS } from '../../locations';
+import { NOT_FOUND, ADMIN_SIGN_IN, ADMIN_LANDING, ADMIN_ROUTES, ADMIN_DRIVERS, ADMIN_USERS, HOME_PAGE } from '../../locations';
 import Loading from "../Loading";
 import PrivateRoute from "./components/PrivateRoute";
 import { ADMIN } from "../../constants/roles";
@@ -21,6 +21,9 @@ export default function ClientRouter() {
         <PrivateRoute exact path={ADMIN_ROUTES()} component={AdminRoutesPage} redirect={ADMIN_SIGN_IN()} roles={[ADMIN]} />
         <PrivateRoute exact path={ADMIN_USERS()} component={AdminUsersPage} redirect={ADMIN_SIGN_IN()} roles={[ADMIN]} />
         <PrivateRoute exact path={ADMIN_DRIVERS()} component={AdminDriversPage} redirect={ADMIN_SIGN_IN()} roles={[ADMIN]} />
+        <Route exact path={HOME_PAGE()}>
+          <Redirect to={ADMIN_SIGN_IN()} />
+        </Route>
         <Route exact path={NOT_FOUND()} component={NotFoundPage} />
         <Route path="*">
           <Redirect to="/404" />
